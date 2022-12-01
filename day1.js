42642773a8a93abc5d1e44bddbfba9aa075012d9
Kevin Batdorf <kbat82@gmail.com>
2022-12-01 14:09:20 -0500

    Refactor

--- day1.js
+++ day1.js
@@ -1,23 +1,21 @@
 import { readFileSync } from 'fs';
 
-const data = readFileSync('data/day1', 'utf-8');
+// [[1, 2, 3], [4, 5, 6]]
+const data = readFileSync('data/day1', 'utf-8')
+    .split('\n\n')
+    .map((elf) => elf.split('\n').map(Number));
 
-const cals = data.split('\n\n').reduce((acc, cur) => {
-    const total = cur.split('\n').reduce((a, b) => Number(a) + Number(b), 0);
+const cals = data.reduce((acc, cur) => {
+    // Add them up
+    const total = cur.reduce((a, b) => a + b, 0);
+    // Only keep the largest
     return total > acc ? total : acc;
 }, 0);
 console.log(cals);
 
 const cals2 = data
-    .split('\n\n')
-    .sort((a, b) => {
-        const current = a
-            .split('\n')
-            .reduce((a, b) => Number(a) + Number(b), 0);
-        const next = b.split('\n').reduce((a, b) => Number(a) + Number(b), 0);
-        return next - current;
-    })
-    .slice(0, 3)
-    .map((c) => c.split('\n').reduce((a, b) => Number(a) + Number(b), 0))
-    .reduce((a, b) => a + b, 0);
+    .map((c) => c.reduce((a, b) => a + b, 0)) // Add insides
+    .sort((a, b) => b - a) // Sort descending
+    .slice(0, 3) // only keep the first 3
+    .reduce((a, b) => a + b, 0); // Add each of the 3 together
 console.log(cals2);
