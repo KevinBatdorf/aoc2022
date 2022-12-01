dbeab9c98390a227301e699ede75eaf1a72a0805
Kevin Batdorf <kbat82@gmail.com>
2022-12-01 14:33:55 -0500

    Update reduce naming

--- day1.js
+++ day1.js
@@ -5,17 +5,17 @@ const data = readFileSync('data/day1', 'utf-8')
     .split('\n\n')
     .map((elf) => elf.split('\n').map(Number));
 
-const cals = data.reduce((acc, cur) => {
+const cals = data.reduce((prev, next) => {
     // Add them up
-    const total = cur.reduce((a, b) => a + b, 0);
+    const total = next.reduce((a, b) => a + b, 0);
     // Only keep the largest
-    return total > acc ? total : acc;
+    return total > prev ? total : prev;
 }, 0);
 console.log(cals);
 
 const cals2 = data
     .map((c) => c.reduce((a, b) => a + b, 0)) // Add insides
     .sort((a, b) => b - a) // Sort descending
-    .slice(0, 3) // only keep the first 3
+    .slice(0, 3) // Only keep the first 3
     .reduce((a, b) => a + b, 0); // Add each of the 3 together
 console.log(cals2);
