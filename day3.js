1e87bd65bd4a51dcecb98823d0335c59a669c1f6
Kevin Batdorf <kbat82@gmail.com>
2022-12-03 10:46:57 -0500

    Add comments

--- day3.js
+++ day3.js
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
 
-// [['vJrwpWtwJgWr', 'hcsFMMfFFhFp'], ['etc', 'etc]]
+// [['vJrwpWtwJgWr', 'hcsFMMfFFhFp'], ['etc', 'etc']]
 const data = readFileSync('data/day3', 'utf-8')
     .trim()
     .split('\n')
@@ -11,25 +11,29 @@ const data = readFileSync('data/day3', 'utf-8')
 
 const go = (data) =>
     data.reduce((total, incoming) => {
+        // Accept unlimited length of arrays to check intersection
         const intersection = incoming
             .map((rucksack) => rucksack.split(''))
             .reduce((a, b) => a.filter((c) => b.includes(c)))[0];
         return (
             total +
+            // Compute the value based on char code
             (intersection === intersection?.toUpperCase()
                 ? intersection.charCodeAt() - 38
                 : intersection.charCodeAt() - 96)
         );
     }, 0);
-const p1 = go(data);
-console.log(p1);
 
 // put every 3 items into an array, rejoin them
+// [['vJrwpWtwJgWrhcsFMMfFFhFp', 'another', 'another'], ['etc', 'etc', 'etc]]
 const data2 = data.reduce((acc, next, i) => {
     return i % 3 === 0
         ? [...acc, [next.join('')]]
         : [...acc.slice(0, -1), [...acc.slice(-1)[0], next.join('')]];
 }, []);
+
+const p1 = go(data);
+console.log(p1);
 
 const p2 = go(data2);
 console.log(p2);
